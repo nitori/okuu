ea3c15c5a63400df82918d3a162f94f147fe2caf
Lars Peter Søndergaard <nitori@chireiden.net>
2015-03-29 23:55:19 +0200

    Added redirect related keys to response.

--- okuu_webpage.py
+++ okuu_webpage.py
@@ -15,21 +15,21 @@ logger = logging.getLogger(__name__)
 class Webpage(BasePlugin):
 
     def check_header(self, url, response):
-        self.request_url = response.url
-        self.was_redirected = False
-        if response.history:
-            self.was_redirected = True
-
+        self.redirected_to_url = response.url
+        self.history_urls = [r.url for r in response.history]
         content_type = response.headers.get('content-type', '')
         if content_type.strip().lower().startswith('text/html'):
             return True
 
     def get_url_info(self, url):
-        response = requests.get(self.request_url)
-        content_type = response.headers.get('content-type', '')
+        # no need to follow redirects once more
+        response = requests.get(self.redirected_to_url)
+        content_type = response.headers.get('content-type', '').lower()
         if 'charset' in content_type:
+            # requests knows what encoding to use
             html_code = response.text
         else:
+            # let beautifulsoup handle the decoding
             html_code = response.content
 
         soup = bs4.BeautifulSoup(html_code)
@@ -39,6 +39,8 @@ class Webpage(BasePlugin):
             name='Webpage',
             type=None,
             infos={
-                'page_title': page_title
+                'page_title': page_title,
+                'history_urls': self.history_urls,
+                'redirected_to_url': self.redirected_to_url,
             }
         )
